2d881d801e2bfc92a77dd842094e45b610fa51d0
ChanduReddy999 <113517229+ChanduReddy999@users.noreply.github.com>
2022-10-21 23:32:35 +0530

    Update discard_remove_pop_hr.py

--- discard_remove_pop_hr.py
+++ discard_remove_pop_hr.py
@@ -1,4 +1,5 @@
 """Discard-remove-pop operations in python (HackerRank Problem)"""
+'''method 1'''
 
 # n = int(input())
 # s = set(map(int, input().split()))      # set of n numbers
@@ -15,24 +16,27 @@
     
 # print(sum(s))
 
-n = int(input())
-s = set(map(int, input().split()))
-N = int(input())
-for i in range(N):
-    cmd = list(map(str, input().split()))
-    if cmd[0] == "pop":
-        s.pop()
-    elif cmd[0] == "remove":
-        try:
-            s.remove(int(cmd[1]))
-        except:
-            continue
-    elif cmd[0] == "discard":
-        try:
-            s.discard(int(cmd[1]))
-        except:
-            continue
-print(sum(s))
+
+'''method 2'''
+
+# n = int(input())
+# s = set(map(int, input().split()))
+# N = int(input())
+# for i in range(N):
+#     cmd = list(map(str, input().split()))
+#     if cmd[0] == "pop":
+#         s.pop()
+#     elif cmd[0] == "remove":
+#         try:
+#             s.remove(int(cmd[1]))
+#         except:
+#             continue
+#     elif cmd[0] == "discard":
+#         try:
+#             s.discard(int(cmd[1]))
+#         except:
+#             continue
+# print(sum(s))
 
 
 '''
